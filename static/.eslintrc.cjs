a853227926298ef683396bafc87417fece166b4d
Maurício Menon <54405514+mauriciomenon@users.noreply.github.com>
2026-03-13 01:48:03 -0300

    build(lint): add eslint8 static fallback config

---
+++ static/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'script',
+  },
+  rules: {
+    'no-empty': 'off',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'no-var': 'off',
+    'prefer-const': 'off',
+  },
+};
